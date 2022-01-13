72c4643c186bb932b739c0ac66c3bdeb8302dab6
MillaPereira <millapms27@gmail.com>
2022-01-13 14:46:10 -0300

    Criaçao da eleiçao na main

--- App.java
+++ App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) {
+
+        Eleicao eleicao = new Eleicao();
     
-        LeituraArquivo.lerArquivoPartidos(args);
+        LeituraArquivo.lerArquivoPartidos(args, eleicao);
         System.out.println("\nCandidatos: \n");
-        LeituraArquivo.lerArquivoCandidatos(args);
+        LeituraArquivo.lerArquivoCandidatos(args, eleicao);
 
     }
 
