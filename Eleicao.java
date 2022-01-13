a79f27af83885d4ed808a90c74987b9143d83d1b
Lara Tagarro Bernardes <74369384+Laratb1@users.noreply.github.com>
2022-01-13 09:44:40 -0300

    classe criada para as eleicoes

--- Eleicao.java
+++ Eleicao.java
@@ -1,7 +1,34 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class Eleicao {
+public class Eleicao{
     private Set<Candidato> candidatos = new HashSet<>();
     private Set<Partido> partidos = new HashSet<>();
+
+    //Getters
+
+    public Set<Candidato> getCandidatos(){
+        return this.candidatos;
+    }
+    public Set<Partido> getPartidos(){
+        return this.partidos;
+    }
+
+    //Setters
+
+    public void setCandidato(Candidato c){
+        this.candidatos.add(c);
+    }
+    public void setPartido(Partido p){
+        this.partidos.add(p);
+    }
+
+    //Functions
+
+    public void imprimeNomePartidos(){
+        for(Partido p : partidos){
+            System.out.print(p.getNomePartido());
+        }
+    }
+
 }
