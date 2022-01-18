cd6939cb8e5933ddad8d05d663483b4a9902b4e4
MillaPereira <millapms27@gmail.com>
2022-01-18 16:54:03 -0300

    Algumas funçoes novas

--- Eleicao.java
+++ Eleicao.java
@@ -27,13 +27,65 @@ public class Eleicao{
 
     public void imprimeNomePartidos(){
         for(Partido p : partidos){
-            System.out.print(p.getNomePartido());
+            System.out.println(p.getNomePartido());
         }
     }
 
     public void imprimeNomeCandidatos(){
         for(Candidato c : candidatos){
-            System.out.print(c.getNomeCandidato());
+            System.out.println(c.getNomeCandidato());
+        }
+    }
+
+    // Especial functions
+
+    public int numeroDeCandidatosEleitos(){
+        int numero = 0;
+        for(Candidato c : candidatos){
+            if(c.getSituacaoCandidato().equals("Eleito")){
+                numero++;
+            }
+        }
+        return numero;
+    }
+
+    public int numeroVotosLegenda(){
+        int numero = 0;
+        for(Partido p : partidos){
+                numero += p.getVotosLegenda();
+        }
+        return numero;
+    }
+
+    public void primeiroUltimoColocadoPorPartido(){
+        Candidato primeiro = new Candidato();
+        Candidato ultimo = new Candidato();
+        int maior = 0;
+        int menor = 100000;
+
+        for(Partido p : partidos){
+            for(Candidato c : candidatos){
+                if(c.getNumeroPartidoCandidato().equals(p.getNumeroPartido())){
+                    if(c.getSituacaoCandidato().equals("Eleito")){
+                        if(c.getVotosNominaisCandidato() > maior){
+                            maior = c.getVotosNominaisCandidato();
+                            primeiro = c;
+                        }
+                        if(c.getVotosNominaisCandidato() < menor){
+                            menor = c.getVotosNominaisCandidato();
+                            ultimo = c;
+                        }
+                    }
+                    else{
+                        System.out.println("Ultimo: " + c.getNomeCandidato());
+                    }
+                }
+            }
+            maior = 0;
+            menor = 100000;
+
+            System.out.println("Primeiro do partido" + p.getNomePartido() + ": " + primeiro.getNomeCandidato());
+            System.out.println("Ultimo do partido" + p.getNomePartido() + ": " + ultimo.getNomeCandidato());
         }
     }
 
